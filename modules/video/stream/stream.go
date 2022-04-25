24af65f7ca174ea09ea506fe95972acda49b6e49
unknown <waynewuweilun@gmail.com>
2022-04-26 02:44:15 +0800

    feat: lab-kafka done

--- modules/video/stream/stream.go
+++ modules/video/stream/stream.go
@@ -39,13 +39,25 @@ func (s *stream) HandleVideoCreated(ctx context.Context, req *pb.HandleVideoCrea
 
 		// [Kafka TODO]
 		// [Describe] Transcode video if get message with scale != 0, you can handle error occurance like above primitive.ObjectIDFromHex(req.GetId()).
+		if err := s.handleVideoWithVariant(ctx, id, variant, req.GetUrl()); err != nil {
+			return nil, &saramakit.HandlerError{Retry: true, Err: err}
+		}
 
 		return &emptypb.Empty{}, nil
 	}
 
 	// [Kafka TODO]
 	// [Describe] Fanout create events to each variant [1080, 720, 480, 320], you can handle error occurance like above primitive.ObjectIDFromHex(req.GetId()).
-
+	variants := []int32{1080, 720, 480, 320}
+	for _, a := range variants{
+		if err := s.produceVideoCreatedWithScaleEvent(&pb.HandleVideoCreatedRequest{
+			Id: req.GetId(),
+			Url: req.GetUrl(),
+			Scale: a,
+		}); err != nil {
+			return nil, &saramakit.HandlerError{Retry: true, Err: err}
+		}
+	}
 	return &emptypb.Empty{}, nil
 }
 
@@ -72,6 +84,8 @@ func (s *stream) produceVideoCreatedWithScaleEvent(req *pb.HandleVideoCreatedReq
 
 	// [Kafka TODO]
 	// [Describe] Send message to kafka
-
+	if err := s.producer.SendMessages(msgs); err != nil {
+		return err
+	}
 	return nil
 }
